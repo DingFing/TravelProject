70818163848161bcbf6c4b5625e06cf4e061affa
JanL <61390304+Hway@users.noreply.github.com>
2020-06-24 15:05:55 +0200

    H

---
+++ Swt2/src/test/java/com/example/Swt2/Swt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Swt2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swt2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
