d0f7dd135b5e2689933ccc00f66efc4a7d858e9e
JanL <61390304+Hway@users.noreply.github.com>
2020-06-29 14:27:30 +0200

    Hallo

--- SWT2/src/main/java/com/example/SWT2/Database/Bewertung.java
+++ SWT2/src/main/java/com/example/SWT2/Database/Bewertung.java
@@ -1,4 +1,35 @@
 package com.example.SWT2.Database;
 
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Bewertung")
 public class Bewertung {
+
+    @JoinColumn(name="UserNr", nullable=false)
+    private User UserNr;
+
+    @JoinColumn(name="ANr", nullable=false)
+    private Aktivity ANr;
+
+    @Column(name= "Bewertung" ,nullable= false)
+    @Type(type= "text")
+    private String Bewertung;
+
+    @Column(name= "Note", precision= 1, scale= 1, nullable= false)
+    private double Note;
+
+    public double getNote(){return Note;}
+    public void setNote(double Note){this.Note= Note;}
+
+    public Aktivity getANr(){return ANr;}
+    public void setANr(Aktivity ANr){this.ANr= ANr;}
+
+    public User getUserNr(){return UserNr;}
+    public void setANr(User UserNr){this.UserNr= UserNr;}
 }
--- SWT2/src/main/java/com/example/SWT2/Database/BietetAn.java
+++ SWT2/src/main/java/com/example/SWT2/Database/BietetAn.java
@@ -1,4 +1,29 @@
 package com.example.SWT2.Database;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "BietetAn")
 public class BietetAn {
+
+    @JoinColumn(name="ReiseNr", nullable=false)
+    private Reise ReiseNr;
+
+    @JoinColumn(name="ANr", nullable=false)
+    private Aktivity ANr;
+
+    @Column(name = "Anzahl", lenght = 1, nullable=false)
+    private Integer Anzahl;
+
+    public void setAnzahl(Integer Anzahl) { this.Anzahl = Anzahl; }
+    public Integer getAnzahl(){return Anzahl;}
+
+    public void setANr(Aktivity ANr) { this.ANr = ANr; }
+    public Aktivity getANr(){return ANr;}
+
+    public void setReiseNr(Reise ReiseNr) { this.ReiseNr = ReiseNr; }
+    public Reise getReiseNr(){return ReiseNr;}
 }
