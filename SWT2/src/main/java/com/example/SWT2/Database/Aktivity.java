589560bdff5059207e83cb9b98985885eb8311e2
JanL <61390304+Hway@users.noreply.github.com>
2020-06-29 17:35:44 +0200

    Hallo

--- SWT2/src/main/java/com/example/SWT2/Database/Aktivity.java
+++ SWT2/src/main/java/com/example/SWT2/Database/Aktivity.java
@@ -1,16 +1,32 @@
 package com.example.SWT2.Database;
 
+import org.hibernate.annotations.Type;
 import javax.persistence.*;
 
 
 @Entity
 @Table(name = "Aktivity")
 public class Aktivity {
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "UserNr" ,length = 10, nullable = false)
-    private Integer UserNr;
+    private Integer ANr;
 
-    @Column(name = "Nachname" ,length = 20, nullable = false)
-    private String Nachname;
+    @Column(name = "Jahreszeit" ,length = 10, nullable = false)
+    private String Jahreszeit;
+
+    @Column(name = "Beschreibung" , nullable = false)
+    @Type(type= "text")
+    private String Beschreibung;
+
+    @Column(name = "Kosten" ,precision= 10, scale= 2, nullable = false)
+    private double Kosten;
+
+    @JoinColumn(name="UnternehmenId", nullable=false)
+    private Unternehmen UnternehmenId;
+
+    @JoinColumn(name="ANr", nullable=false)
+    private Aktivity Alternativ;
 }
