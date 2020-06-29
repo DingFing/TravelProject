2521cd245fb8df136cb84b876d8d30d9519dd6e6
JanL <61390304+Hway@users.noreply.github.com>
2020-06-29 13:05:42 +0200

    Database

---
+++ SWT2/src/main/java/com/example/SWT2/Database/User.java
@@ -0,0 +1,47 @@
+package com.example.SWT2.Database;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "User")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "UserNr" ,length = 10, nullable = false)
+    private Integer UserNr;
+
+    @Column(name = "Nachname" ,length = 20, nullable = false)
+    private String Nachname;
+
+    @Column(name = "Vorname" ,length = 20, nullable = false)
+    private String Vorname;
+
+    @Column(name = "GeburtsDat" ,nullable = false)
+    private Date GeburtsDat;
+
+    @Column(name = "Password" ,length = 30, nullable = false)
+    private String Password;
+
+    @Column(name = "KontoNr" ,length = 10, nullable = false)
+    private Integer KontoNr;
+
+    public void setNachname(String Nachname){
+        this.Nachname = Nachname;
+    }
+    public String getNachname(){ return this.Nachname; }
+
+    public void setVorname(String Vorname){this.Vorname = Vorname;}
+    public String getVorname(){ return this.Vorname; }
+
+    public String getUserNr(){ return this.Vorname; }
+
+    public void setGeburtsDat(Date GeburtsDat){this.GeburtsDat = GeburtsDat;}
+    public Date getGeburtsDat(){ return this.GeburtsDat; }
+
+    public void setKontoNr(Integer KontoNr){this.KontoNr = KontoNr;}
+    public Integer getKontoNr(){ return this.KontoNr; }
+
+    public String setPassword(String Password){return this.Password;}
+    public void getPassword(){return this.Password}
+}
--- SWT2/src/main/java/com/example/SWT2/Swt2Application.java
+++ SWT2/src/main/java/com/example/SWT2/Swt2Application.java
@@ -1,13 +1,24 @@
 package com.example.SWT2;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.DriverManager;
 
 @SpringBootApplication
 public class Swt2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Swt2Application.class, args);
-	}
 
+	}
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Das ist eine Ausgabe");
+		};
+	}
 }
