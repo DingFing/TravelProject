2b7e4cca6385a0089304932cceccaab0a850d5d1
JanL <61390304+Hway@users.noreply.github.com>
2020-06-30 17:37:18 +0200

    S

--- SWT2/src/main/java/com/example/SWT2/Database/DatabaseManager.java
+++ SWT2/src/main/java/com/example/SWT2/Database/DatabaseManager.java
@@ -1,6 +1,6 @@
 package com.example.SWT2.Database;
 
-import com.example.SWT2.Database.Tables.User;
+import com.example.SWT2.Database.Tables.*;
 import org.hibernate.*;
 
 import org.hibernate.cfg.*;
@@ -8,7 +8,7 @@ import org.hibernate.cfg.*;
 public class DatabaseManager {
 
     private static SessionFactory sf;
-    public Integer adduser(String Nachname, String Vorname, java.sql.Date GeburtsDat, String Password, Integer KontoNr){
+    public Integer adduser(String Nachname, String Vorname, java.sql.Date GeburtsDat, String Password, Integer KontoNr, Integer Rolle){
         Session s = sf.openSession();
         Transaction tc = null;
         Integer UserNr = null;
@@ -20,6 +20,7 @@ public class DatabaseManager {
             user.setGeburtsDat(GeburtsDat);
             user.setPassword(Password);
             user.setKontoNr(KontoNr);
+            user.setRolle(Rolle);
             UserNr = (Integer) s.save(user);
             tc.commit();
         }
@@ -30,5 +31,70 @@ public class DatabaseManager {
             s.close();
         }
         return UserNr;
+    }
+
+    public Integer AddReise(String Beschreibung, String Ort, String Region, String Land,Double Preis, String Jahreszeit){
+        Session s = sf.openSession();
+        Transaction tc = null;
+        Integer ReiseNr = null;
+        try{
+            tc = s.beginTransaction();
+            Reise reis = new Reise();
+            reis.setBeschreibung(Beschreibung);
+            reis.setOrt(Ort);
+            reis.setRegion(Region);
+            reis.setLand(Land);
+            reis.setPreis(Preis);
+            reis.setJahreszeit(Jahreszeit);
+            ReiseNr = (Integer) s.save(reis);
+            tc.commit();
+        }
+        catch(HibernateException ex){
+            if(tc!= null) tc.rollback();
+            ex.printStackTrace();
+        }finally{
+            s.close();
+        }
+        return ReiseNr;
+    }
+
+    public Integer AddUnternehmen(Integer KontoNr){
+        Session s = sf.openSession();
+        Transaction tc = null;
+        Integer UnternehmenNr = null;
+        try{
+            tc = s.beginTransaction();
+            Unternehmen un = new Unternehmen();
+            un.setKontoNr(KontoNr);
+            UnternehmenNr = (Integer) s.save(un);
+            tc.commit();
+        }
+        catch(HibernateException ex){
+            if(tc!= null) tc.rollback();
+            ex.printStackTrace();
+        }finally{
+            s.close();
+        }
+        return UnternehmenNr;
+    }
+
+    public Integer AddAktivity(Integer KontoNr){
+        Session s = sf.openSession();
+        Transaction tc = null;
+        Integer UnternehmenNr = null;
+        try{
+            tc = s.beginTransaction();
+            Unternehmen un = new Unternehmen();
+            un.setKontoNr(KontoNr);
+            UnternehmenNr = (Integer) s.save(un);
+            tc.commit();
+        }
+        catch(HibernateException ex){
+            if(tc!= null) tc.rollback();
+            ex.printStackTrace();
+        }finally{
+            s.close();
+        }
+        return UnternehmenNr;
     }
 }
